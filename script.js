690d9fbc6635e35441696491906d92ac0243de94
David <popadavid843@yahoo.com>
2021-12-07 22:11:33 +0200

    List add/remove

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 "use strit";
-const btn = document.getElementById("btn");
-const inputTxt = document.getElementById("inputTxt");
-const allItems = document.getElementById("allItems");
+const inputText = document.querySelector("#txt");
+const btn = document.querySelector(".btn-list");
+const list = document.querySelector(".container ul");
 
-btn.addEventListener("click", function (e) {
-  if (inputTxt.value != "") {
-    e.preventDefault();
-    const li = document.createElement("li");
-    li.innerHTML = inputTxt.value;
-    allItems.appendChild(li);
-    inputTxt.value = "";
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (inputText.value != "") {
+    //create li
+    const myLi = document.createElement("li");
+    myLi.innerHTML = inputText.value;
+    list.appendChild(myLi);
+
+    //create span
+    const span = document.createElement("span");
+    span.innerHTML = "x";
+    myLi.appendChild(span);
+  }
+  inputText.value = "";
+  const close = document.querySelectorAll("span");
+  for (let i = 0; i < close.length; i++) {
+    close[i].addEventListener("click", () => {
+      setTimeout(() => {
+        close[i].parentElement.remove();
+      }, 500);
+    });
   }
 });
