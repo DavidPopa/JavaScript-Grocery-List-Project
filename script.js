d45781aa6f209e974c29dc4d8c2987112c2f50a3
David <popadavid843@yahoo.com>
2021-12-07 22:14:50 +0200

    update

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 "use strit";
 const inputText = document.querySelector("#txt");
 const btn = document.querySelector(".btn-list");
-const list = document.querySelector(".container ul");
+const list = document.querySelector(".container");
 
 btn.addEventListener("click", (e) => {
   e.preventDefault();
   if (inputText.value != "") {
     //create li
+    const ul = document.createElement("ul");
     const myLi = document.createElement("li");
     myLi.innerHTML = inputText.value;
+    list.appendChild(ul);
     list.appendChild(myLi);
 
     //create span
     const span = document.createElement("span");
     span.innerHTML = "x";
+    // ul.appendChild(span);
     myLi.appendChild(span);
   }
   inputText.value = "";
